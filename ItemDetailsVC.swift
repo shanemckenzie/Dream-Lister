051ac751e50252b960c94136850270cb478c1c01
Shane McKenzie <shane.mckenzie59@gmail.com>
2016-10-19 20:27:14 -0600

    Edit existing items

--- ItemDetailsVC.swift
+++ ItemDetailsVC.swift
@@ -17,6 +17,7 @@ class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDeleg
     @IBOutlet weak var detailsField: CustomTextField!
     
     var stores = [Store]()
+    var itemToEdit: Item?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,6 +47,12 @@ class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDeleg
 //        ad.saveContext()
         
         getStores()
+        
+        if itemToEdit != nil {
+            
+            loadItemData()
+            
+        }
         
     }
 
@@ -85,7 +92,17 @@ class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDeleg
     
     @IBAction func savePressed(_ sender: UIButton) {
         
-        let item = Item(context: context)
+        var item: Item!
+        
+        if itemToEdit == nil {
+            
+            item = Item(context: context)
+            
+        } else {
+            
+            item = itemToEdit
+            
+        }
         
         if let title = titleField.text {
             item.title = title
@@ -107,6 +124,32 @@ class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDeleg
         ad.saveContext()
         
         _ = navigationController?.popViewController(animated: true)
+        
+    }
+    
+    func loadItemData() {
+        
+        if let item = itemToEdit {
+            titleField.text = item.title
+            PriceField.text = "\(item.price)"
+            detailsField.text = item.details
+            
+            if let store = item.toStore {
+                
+                var index = 0
+                repeat {
+                    
+                    let s = stores[index]
+                    if s.name == store.name {
+                        storePicker.selectRow(index, inComponent: 0, animated: false)
+                        break
+                    }
+                    
+                    index += 1
+                    
+                } while (index < stores.count)
+            }
+        }
         
     }
     
